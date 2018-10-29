2bedc0150fd82673484b022acec0e24dba7f03a1
agepere <aperez.fr2@gmail.com>
2018-10-29 16:35:58 +0100

    Practica acabada

--- Practica 3/Practica3/Practica3.py
+++ Practica 3/Practica3/Practica3.py
@@ -85,12 +85,79 @@ def list_bd():
     lb.pack(side=LEFT,fill=BOTH)
     sc.config(command= lb.yview)
     
-def buscarTitulo(text):
+def popularesBD():
+    conn= sqlite3.connect("foro.db")
+    conn.text_factory= str
+    
+    cursor=conn.execute("""SELECT * FROM FORO ORDER BY VISITAS DESC LIMIT 5""")
+    tp=Toplevel(height=200,width=150)
+    sc= Scrollbar(tp)
+    sc.pack(side=RIGHT, fill=Y)
+    lb=Listbox(tp, width=200, height=150, yscrollcommand=sc.set)
+    
+    for row in cursor:
+        lb.insert(END, "Titulo :" + row[1])
+        lb.insert(END, "Autor: "+ row[2])
+        lb.insert(END, "Fecha: "+ row[3])
+        lb.insert(END, "Respuestas: "+ str(row[5]))
+        lb.insert(END, "Visitas: "+ str(row[6]))
+        lb.insert(END,'')
+          
+    
+    
+   
+    lb.pack(side=LEFT,fill=BOTH)
+    sc.config(command= lb.yview)
+    
+def activosBD():
+    conn= sqlite3.connect("foro.db")
+    conn.text_factory= str
+    
+    cursor=conn.execute("""SELECT * FROM FORO ORDER BY RESPUESTAS DESC LIMIT 10 """)
+    tp=Toplevel(height=200,width=150)
+    sc= Scrollbar(tp)
+    sc.pack(side=RIGHT, fill=Y)
+    lb=Listbox(tp, width=200, height=150, yscrollcommand=sc.set)
+    
+    for row in cursor:
+        lb.insert(END, "Titulo :" + row[1])
+        lb.insert(END, "Autor: "+ row[2])
+        lb.insert(END, "Fecha: "+ row[3])
+        lb.insert(END, "Respuestas: "+ str(row[5]))
+        lb.insert(END, "Visitas: "+ str(row[6]))
+        lb.insert(END,'')
+          
+    
+    
+   
+    lb.pack(side=LEFT,fill=BOTH)
+    sc.config(command= lb.yview)
+
+def buscar_BD_Titulo():   
+    def buscarTitulo(event):
+        s=en.get()
+        encontrarTitulo(str(s))
+        v.destroy()
+        
+    v= Toplevel()
+    lb=Label(v,text="Introduzca el tema")
+    lb.pack(side=LEFT)
+    en= Entry(v)
+    en.bind("<Return>",buscarTitulo)
+    en.pack()
+    
+        
+    
+    
+def encontrarTitulo(text):
+    
     
     conn= sqlite3.connect("foro.db")
     conn.text_factory= str
     
     cursor=conn.execute("SELECT * FROM FORO WHERE TITULO LIKE '%"+text+"%'")
+    
+    
     tp=Toplevel(height=200,width=150)
     sc= Scrollbar(tp)
     sc.pack(side=RIGHT, fill=Y)
@@ -104,11 +171,134 @@ def buscarTitulo(text):
           
     
     
-   
+    conn.close()   
     lb.pack(side=LEFT,fill=BOTH)
     sc.config(command= lb.yview)
     
     
+def buscar_BD_Autor():   
+    def buscarAutor(event):
+        s=en.get()
+        encontrarAutor(str(s))
+        v.destroy()
+        
+    v= Toplevel()
+    lb=Label(v,text="Introduzca el tema")
+    lb.pack(side=LEFT)
+    en= Entry(v)
+    en.bind("<Return>",buscarAutor)
+    en.pack()
+    
+        
+    
+    
+def encontrarAutor(text):
+    
+    
+    conn= sqlite3.connect("foro.db")
+    conn.text_factory= str
+    
+    cursor=conn.execute("SELECT * FROM FORO WHERE AUTOR LIKE '%"+text+"%'")
+    
+    
+    tp=Toplevel(height=200,width=150)
+    sc= Scrollbar(tp)
+    sc.pack(side=RIGHT, fill=Y)
+    lb=Listbox(tp, width=200, height=150, yscrollcommand=sc.set)
+    
+    for row in cursor:
+        lb.insert(END, "Titulo :" + row[1])
+        lb.insert(END, "Autor: "+ row[2])
+        lb.insert(END, "Fecha: "+ row[3])
+        lb.insert(END,'')
+          
+    
+    
+    conn.close()   
+    lb.pack(side=LEFT,fill=BOTH)
+    sc.config(command= lb.yview)
+    
+    
+    
+def buscar_BD_Fecha(error=False):   
+    def buscarFecha(event):
+       
+        diaS=dia.get()
+        mesS=mes.get()
+        anhoS=anho.get()
+        
+        
+        
+        if(len(diaS.strip())==0 or len(mesS.strip())==0 or len(anhoS.strip())==0 ):
+            v.destroy()
+            buscar_BD_Fecha(True)
+        else:
+            v.destroy()
+            encontrarFecha(diaS+"/"+mesS+"/"+anhoS)
+        
+    
+       
+        
+        
+    v= Toplevel()
+    lb=Label(v,text="Introduzca el dia (dd)")
+    lb.grid(row=0,column=0)
+    dia= Entry(v, width=2)
+    dia.bind("<Return>",buscarFecha)
+    dia.grid(row=0,column=1)
+    
+    lb=Label(v,text="Introduzca el mes (MM)")
+    lb.grid(row=1,column=0)
+    mes= Entry(v, width=2)
+    mes.bind("<Return>",buscarFecha)
+    mes.grid(row=1,column=1)
+    
+    lb=Label(v,text="Introduzca el año (yyyy)")
+    lb.grid(row=2,column=0)
+    anho= Entry(v,width=4)
+    anho.bind("<Return>",buscarFecha)
+    anho.grid(row=2,column=1)
+    
+    if(error):
+        var= StringVar()
+        label= Message(v, textvariable=var, relief=FLAT, fg='red')
+        var.set("Rellena los campos perro")
+        label.grid(row=3,column=0)
+        
+        
+    
+    
+    
+        
+    
+    
+def encontrarFecha(text):
+    
+    conn= sqlite3.connect("foro.db")
+    conn.text_factory= str
+    
+    cursor=conn.execute("SELECT * FROM FORO WHERE FECHA LIKE '%"+text+"%' ")
+    
+    
+    tp=Toplevel(height=200,width=150)
+    sc= Scrollbar(tp)
+    sc.pack(side=RIGHT, fill=Y)
+    lb=Listbox(tp, width=200, height=150, yscrollcommand=sc.set)
+    
+    for row in cursor:
+        lb.insert(END, "Titulo :" + row[1])
+        lb.insert(END, "Autor: "+ row[2])
+        lb.insert(END, "Fecha: "+ row[3])
+        lb.insert(END,'')
+          
+    
+    
+    conn.close()   
+    lb.pack(side=LEFT,fill=BOTH)
+    sc.config(command= lb.yview)    
+
+
+
 
 def mainMetodo():
     crearBD()
@@ -127,7 +317,7 @@ def mainMetodo():
             respuestas= extraerRespuestas(i)
             visitas= extraerVisitas(i)
             
-            insertRow(titulo, autor, fecha, enlace, respuestas, visitas)
+            insertRow(titulo, autor, fecha, enlace, respuestas.replace(',',''), visitas.replace(',',''))
      
      
     conn= sqlite3.connect("foro.db")
@@ -161,12 +351,25 @@ def ventana():
     mb1.menu =Menu(mb1, tearoff = 0 )
     mb1["menu"]=mb1.menu
 
-    mb1.menu.add_command(label="Almacenar",command=buscarTitulo)
+    mb1.menu.add_command(label="Tema",command=buscar_BD_Titulo)
+    mb1.menu.add_command(label="Autor",command=buscar_BD_Autor)
+    mb1.menu.add_command(label="Fecha",command=buscar_BD_Fecha)
    
 
     mb1.grid(row=0,column=1)
     
     
+    mb1=Menubutton(bottomframe, text="Estadisticas", relief=RAISED )
+    mb1.menu =Menu(mb1, tearoff = 0 )
+    mb1["menu"]=mb1.menu
+
+    mb1.menu.add_command(label="Temas más populares",command=popularesBD)
+    mb1.menu.add_command(label="Temas más activos",command=activosBD)
+   
+
+    mb1.grid(row=0,column=2)
+    
+    #DESC
     
     root.mainloop()
     
