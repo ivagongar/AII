71635f0ef8f4363eaea60b8bed381b63197cb627
agepere <angperllo@alum.us.es>
2018-12-25 20:36:50 +0100

    Finished models

--- Proyecto/plAIIgames/app/models.py
+++ Proyecto/plAIIgames/app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Genre(models.Model):
+    """
+    Géneros de los juegos, sólo tendrán un campo 'name' que sera único para evitar que cuando leamos géneros de dos plataformas diferentes se creen más de uno igual
+    """
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class OfferCategory(models.Model):
+    """
+    Categoría de la oferta:
+    En el caso de ps si miras su store en rebajas y ofertas habrá diferentes tipos, para saber a que tipo pertenece cada uno habrá que recorrerse todos los juegos que tiene e irlos añadiendo
+    """
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    """
+    Juego, atributos conflictivos:
+        - type: Indica la plataforma PS4,PS vita, PS3, Nintendo...
+        - plus_cost: Si por estar suscrito a su plataforma como en el caso de la ps plus la rebaja es mayor
+    """
+    title = models.TextField(unique=True)
+    description = models.TextField()
+    type = models.TextField()
+    rating = models.IntegerField()
+    cost = models.FloatField()
+    on_sale_cost = models.FloatField()
+    plus_cost = models.FloatField()
+    start_date_on_sale = models.DateTimeField()
+    end_date_on_sale = models.DateTimeField()
+    release_date = models.DateTimeField()
+
+    genres = models.ManyToManyField(Genre, related_name='games')
+    offer_categories = models.ManyToManyField(OfferCategory, related_name='games')
+
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    """
+      Librerías para guardar tus juegos
+    """
+    title = models.TextField()
+    description = models.TextField()
+
+    games = models.ManyToManyField(Game, related_name='libraries')
+    user = models.ForeignKey(User, related_name="library", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
