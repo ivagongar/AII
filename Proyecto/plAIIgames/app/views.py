6555f5e11d23dbba823ffbe135c9e64adbfd82a8
agepere <aperez.fr2@gmail.com>
2019-01-17 17:50:01 +0100

    Logeo + registro

--- Proyecto/plAIIgames/app/views.py
+++ Proyecto/plAIIgames/app/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from app.models import Genre, Game
 from app import funciones
+
 
 # Create your views here.
 
 def index(request):
-    return render(request,'app/index.html')
+    try:
+        auth = request.user.username
+    except:
+        auth = None
+
+    return render(request, 'app/index.html', {"username": auth})
+
 
 def populateSwitch(request):
     funciones.almacenarSwitch(3)
-    
-    return render(request,'app/index.html')
+
+    return render(request, 'app/index.html')
+
 
 def mostrarJuegos(request):
     juegos = Game.objects.all()
-    
-    return render(request,'app/listAll.html',{"games":juegos})
+
+    return render(request, 'app/listAll.html', {"games": juegos})
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/signUp.html', {'form': form})
--- Proyecto/plAIIgames/plAIIgames/urls.py
+++ Proyecto/plAIIgames/plAIIgames/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('app/', include('app.urls')),
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
